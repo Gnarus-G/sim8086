72d337d2a7f34d6b1634c837c8d6ecae7a33e3a3
gnarus-g <37311893+Gnarus-G@users.noreply.github.com>
2023-09-30 22:17:59 -0400

    refactor: decode module

---
+++ src/decode.rs
@@ -0,0 +1,75 @@
+#[derive(Debug, PartialEq)]
+enum Opcode {
+    Mov = 0b100010,
+}
+
+fn get_opcode(byte: &u8) -> Option<Opcode> {
+    let first_six_bits = byte >> 2;
+    match first_six_bits {
+        0b100010 => Some(Opcode::Mov),
+        _ => None,
+    }
+}
+
+fn get_reg_name((reg, w_set): (u8, u8)) -> &'static str {
+    match (reg, w_set) {
+        (0, 0) => "al",
+        (0, 1) => "ax",
+        (1, 0) => "cl",
+        (1, 1) => "cx",
+        (2, 0) => "dl",
+        (2, 1) => "dx",
+        (3, 0) => "bl",
+        (0b11, 1) => "bx",
+        (0b100, 0) => "ah",
+        (0b100, 1) => "sp",
+        (0b101, 0) => "ch",
+        (0b101, 1) => "bp",
+        (0b110, 0) => "dh",
+        (0b110, 1) => "si",
+        (0b111, 0) => "bh",
+        (0b111, 1) => "di",
+        _ => todo!(),
+    }
+}
+
+pub fn decode(buffer: &[u8]) -> Vec<String> {
+    let mut res = vec![];
+
+    buffer.windows(2).for_each(|a| {
+        if let [h, l] = a {
+            if let Some(text) = decode_instruction(h, l) {
+                res.push(text);
+            }
+        }
+    });
+
+    res
+}
+
+fn decode_instruction(high: &u8, low: &u8) -> Option<String> {
+    if let Some(Opcode::Mov) = get_opcode(high) {
+        let d_mask: u8 = 0b00000010;
+        let w_mask: u8 = 0b00000001;
+
+        let w_set = w_mask & high;
+
+        let reg_is_destination = (d_mask & high) == 1;
+        let reg = (low & 0b00111000) >> 3;
+        let rm_reg = low & 0b00000111;
+
+        let reg_name = get_reg_name((reg, w_set));
+
+        let rm_reg_name = get_reg_name((rm_reg, w_set));
+
+        let text = if reg_is_destination {
+            format!("mov {}, {}", reg_name, rm_reg_name)
+        } else {
+            format!("mov {}, {}", rm_reg_name, reg_name)
+        };
+
+        Some(text)
+    } else {
+        None
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use std::fs;
 
+use crate::decode::decode;
+
+mod decode;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let path = std::env::args()
         .nth(1)
@@ -9,65 +13,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     println!("bits 16\n");
 
-    buffer.windows(2).for_each(|a| {
-        if let [h, l] = a {
-            if let Some(Opcode::Mov) = get_opcode(h) {
-                let d_mask: u8 = 0b00000010;
-                let w_mask: u8 = 0b00000001;
-
-                let w_set = w_mask & h;
-
-                let reg_is_destination = (d_mask & h) == 1;
-                let reg = (l & 0b00111000) >> 3;
-                let rm_reg = l & 0b00000111;
-
-                let reg_name = get_reg_name((reg, w_set));
-
-                let rm_reg_name = get_reg_name((rm_reg, w_set));
-
-                if reg_is_destination {
-                    println!("mov {}, {}", reg_name, rm_reg_name);
-                } else {
-                    println!("mov {}, {}", rm_reg_name, reg_name);
-                }
-            }
-        }
-    });
+    for i in decode(&buffer) {
+        println!("{}", i);
+    }
 
     Ok(())
-}
-
-#[derive(Debug, PartialEq)]
-enum Opcode {
-    Mov = 0b100010,
-}
-
-fn get_opcode(byte: &u8) -> Option<Opcode> {
-    let first_six_bits = byte >> 2;
-    match first_six_bits {
-        0b100010 => Some(Opcode::Mov),
-        _ => None,
-    }
-}
-
-fn get_reg_name((reg, w_set): (u8, u8)) -> &'static str {
-    match (reg, w_set) {
-        (0, 0) => "al",
-        (0, 1) => "ax",
-        (1, 0) => "cl",
-        (1, 1) => "cx",
-        (2, 0) => "dl",
-        (2, 1) => "dx",
-        (3, 0) => "bl",
-        (0b11, 1) => "bx",
-        (0b100, 0) => "ah",
-        (0b100, 1) => "sp",
-        (0b101, 0) => "ch",
-        (0b101, 1) => "bp",
-        (0b110, 0) => "dh",
-        (0b110, 1) => "si",
-        (0b111, 0) => "bh",
-        (0b111, 1) => "di",
-        _ => todo!(),
-    }
 }
