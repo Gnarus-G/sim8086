a9ba7e2779b79ed07520af60fc09ea8c877c5e4d
gnarus-g <37311893+Gnarus-G@users.noreply.github.com>
2023-10-08 23:48:06 -0400

    feat: snapshot tests for exec

---
+++ tests/exec.rs
@@ -0,0 +1,30 @@
+use std::process::Command;
+
+use insta::assert_display_snapshot;
+
+macro_rules! test_with {
+    ($file:literal) => {
+        let _ = Command::new("nasm")
+            .arg(format!("./fixtures/exec/{}.asm", $file))
+            .status()
+            .unwrap();
+
+        let app_output = assert_cmd::Command::cargo_bin("sim8086")
+            .unwrap()
+            .arg(format!("./fixtures/exec/{}", $file))
+            .arg("--exec")
+            .output()
+            .map(|out| String::from_utf8(out.stdout).unwrap())
+            .unwrap();
+
+        insta::with_settings!({ description => $file }, {
+            assert_display_snapshot!(app_output);
+        })
+    };
+}
+
+#[test]
+fn movs() {
+    test_with!("immediate_movs");
+    test_with!("register_movs");
+}
